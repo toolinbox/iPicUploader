fa4c64fe294280b158988814f76514080803347f
Jason <quietjason@gmail.com>
2016-09-01 15:37:10 +0800

    Init iPicUploader

---
+++ Example/Pods/Target Support Files/Pods-iPicUploader_Tests/Pods-iPicUploader_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_iPicUploader_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_iPicUploader_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/iPicUploader/iPicUploader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iPicUploader : NSObject
+@end
+@implementation PodsDummy_iPicUploader
+@end
