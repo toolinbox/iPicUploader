f0f9d86f59529fe8c3032226098acbcda2a71680
Jason <quietjason@gmail.com>
2016-09-01 15:47:04 +0800

    Add MainWindowController.

---
+++ Example/iPicUploaderExample/MainWindowController.swift
@@ -0,0 +1,25 @@
+//
+//  MainWindowController.swift
+//  iPicUploadImageDemo
+//
+//  Created by Jason Zheng on 8/31/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+  override var windowNibName: String? {
+    return "MainWindowController"
+  }
+  
+  override func windowDidLoad() {
+    super.windowDidLoad()
+    
+    let imageFilePath = "/Users/jason/Downloads/1.jpg"
+//    iPic.uploadImage(imageFilePath) { (imageLink, error) in
+//      print(imageLink)
+//      print(error)
+//    }
+  }
+}
