3a21f79d0d72657552b3befd86fd277258b12e10
Jason <quietjason@gmail.com>
2016-09-01 15:42:50 +0800

    Add MainWindowController.

--- Example/iPicUploaderExample/AppDelegate.swift
+++ Example/iPicUploaderExample/AppDelegate.swift
@@ -10,18 +10,14 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-  @IBOutlet weak var window: NSWindow!
-
+  var mainWindowController: MainWindowController?
 
   func applicationDidFinishLaunching(aNotification: NSNotification) {
-    // Insert code here to initialize your application
+      
+    let mainWindowController = MainWindowController()
+    mainWindowController.showWindow(self)
+    
+    self.mainWindowController = mainWindowController
   }
-
-  func applicationWillTerminate(aNotification: NSNotification) {
-    // Insert code here to tear down your application
-  }
-
-
 }
 
