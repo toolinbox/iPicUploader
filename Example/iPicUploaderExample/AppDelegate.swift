0412374c8adc7f98eaa80eb3405caec45ec2507b
Jason <quietjason@gmail.com>
2016-09-01 15:40:45 +0800

    Add iPicUploaderExample.

---
+++ Example/iPicUploaderExample/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  iPicUploaderExample
+//
+//  Created by Jason Zheng on 9/1/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
