740ea229b3233cc665b2afa694f115f197d4877d
Jason <quietjason@gmail.com>
2016-09-03 16:06:54 +0800

    Add version and json in iPicImage.

--- Example/Pods/iPicUploaderTests/iPicImageTests.swift
+++ Example/Pods/iPicUploaderTests/iPicImageTests.swift
@@ -15,11 +15,16 @@ class iPicImageTests: XCTestCase {
     let imageFilePath = "/Users/ipic/Downloads/pic.jpg"
     let image1 = iPicImage(imageFilePath: imageFilePath)
     XCTAssertTrue(!image1.id.isEmpty)
+    XCTAssertEqual(image1.version, 1)
+    
+    image1.json = NSUUID().UUIDString
     
     let data = NSKeyedArchiver.archivedDataWithRootObject(image1)
     let image2 = NSKeyedUnarchiver.unarchiveObjectWithData(data) as! iPicImage
     XCTAssertEqual(image1.id, image2.id)
     XCTAssertEqual(image1.imageFilePath, image2.imageFilePath)
     XCTAssertEqual(image1.imageData?.length, image2.imageData?.length)
+    XCTAssertEqual(image1.version, image2.version)
+    XCTAssertEqual(image1.json as? String, image2.json as? String)
   }
 }
--- iPicUploader/Classes/iPicImage.swift
+++ iPicUploader/Classes/iPicImage.swift
@@ -18,9 +18,16 @@ public class iPicImage: NSObject, NSCoding {
   private static let imageFilePathKey = "imageFilePath"
   private static let imageDataKey = "imageData"
   
+  private static let versionKey = "version"
+  private static let jsonKey = "json"
+  
   public var id = NSUUID().UUIDString
   public var imageFilePath: String?
   public var imageData: NSData?
+  
+  public var version = 1
+  public var json: AnyObject?
+  
   var handler: iPicUploadHandler?
   
   public init(imageFilePath: String) {
@@ -43,11 +50,17 @@ public class iPicImage: NSObject, NSCoding {
     id = (aDecoder.decodeObjectForKey(iPicImage.idKey) as? String) ?? ""
     imageFilePath = (aDecoder.decodeObjectForKey(iPicImage.imageFilePathKey) as? String) ?? ""
     imageData = (aDecoder.decodeObjectForKey(iPicImage.imageDataKey) as? NSData) ?? nil
+    
+    version = aDecoder.decodeIntegerForKey(iPicImage.versionKey)
+    json = aDecoder.decodeObjectForKey(iPicImage.jsonKey)
   }
   
   public func encodeWithCoder(aCoder: NSCoder) {
     aCoder.encodeObject(id, forKey: iPicImage.idKey)
     aCoder.encodeObject(imageFilePath, forKey: iPicImage.imageFilePathKey)
     aCoder.encodeObject(imageData, forKey: iPicImage.imageDataKey)
+    
+    aCoder.encodeInteger(version, forKey: iPicImage.versionKey)
+    aCoder.encodeObject(json, forKey: iPicImage.jsonKey)
   }
 }
